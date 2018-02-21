6e70d3f8362502ca079953d70ea03dc58e9a0e1c
Eduardo <eduamaya@ucm.es>
2018-02-21 11:49:45 +0100

    terminada primera versión Vehicle

--- src/main/java/es/ucm/fdi/model/Road.java
+++ src/main/java/es/ucm/fdi/model/Road.java
@@ -3,7 +3,11 @@ package es.ucm.fdi.model;
 //clase para que funcione
 public class Road {
 		private int length;
+		protected String id;
 
+		public Road(){
+		}
+		
 		public int getLength() {
 			return length;
 		}
--- src/main/java/es/ucm/fdi/model/Vehicle.java
+++ src/main/java/es/ucm/fdi/model/Vehicle.java
@@ -1,9 +1,16 @@
 package es.ucm.fdi.model;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.util.List;
+
+import es.ucm.fdi.ini.Ini;
+import es.ucm.fdi.ini.IniSection;
 
 public class Vehicle {
 
+	private String id;
 	private int maxSpeed;
 	private int currentSpeed;
 	private Road road;
@@ -11,9 +18,12 @@ public class Vehicle {
 	private List<Junction> itinerary;
 	private int breakdownTime;
 	private boolean hasArrived;
+	private int kilometrage;
 
-	public Vehicle() {
-
+	public Vehicle(String id, int maxSpeed, Road road) {
+		this.id = id;
+		this.maxSpeed = maxSpeed;
+		this.road = road;
 	}
 
 	public void advance() {
@@ -23,6 +33,7 @@ public class Vehicle {
 			int newLocation = location + currentSpeed;
 			location = (newLocation >= road.getLength()) ? road.getLength()
 					: newLocation;
+			kilometrage += currentSpeed;
 		}
 	}
 
@@ -52,6 +63,23 @@ public class Vehicle {
 	}
 
 	public String generateReport() {
-		throw new UnsupportedOperationException();
+
+		Ini ini = new Ini();
+		IniSection sec;
+
+		sec = new IniSection("vehicle_report");
+		sec.setValue("id", id);
+		sec.setValue("time", 0);
+		sec.setValue("speed", currentSpeed);
+		sec.setValue("kilometrage", kilometrage);
+		sec.setValue("faulty", breakdownTime);
+		String loc = (location == road.getLength()) ? "arrived" : "(" + road.id
+				+ "," + location + ")";
+		sec.setValue("location", loc);
+
+		ini.addsection(sec);
+
+		return ini.toString();
 	}
+	
 }
