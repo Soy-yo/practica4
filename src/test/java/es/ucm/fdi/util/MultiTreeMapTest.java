ac1f65552a9c6822eaa1ba61e3d08d5a00e1d80b
Pablo <pasanz10@ucm.es>
2018-02-22 16:22:33 +0100

    Junction creado

--- src/test/java/es/ucm/fdi/util/MultiTreeMapTest.java
+++ src/test/java/es/ucm/fdi/util/MultiTreeMapTest.java
@@ -5,111 +5,112 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.Comparator;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by mfreire on 12/01/18.
  */
 public class MultiTreeMapTest {
 
-    /**
-     * A simple test class with 2 fields, only the first of which is
-     * used to provide ordering
-     */
-    private static class T implements Comparable<T> {
-        private int i;
-        private String s;
+  @Test
+  public void add() throws Exception {
 
-        public T(int i, String s) {
-            this.i = i;
-            this.s = s;
-        }
+  }
 
-        @Override
-        public int compareTo(T o) {
-            return o.i - i;
-        }
+  @Test
+  public void innerValuesAscending() throws Exception {
+    MultiTreeMap<Integer, T> ts = new MultiTreeMap<>();
+    T[] array = new T[]{
+        new T(3, "3"),
+        new T(1, "1"),
+        new T(2, "2"),
+        new T(3, "3.1"),
+        new T(1, "1.1"),
+        new T(2, "2.1"),
+        new T(3, "3.2"),
+        new T(0, "0"),
+        new T(2, "2.2")};
 
-        public String toString() {
-            return "{" + i + ":" + s + "}";
-        }
+    for (T t : array) ts.putValue(t.i, t);
+    Arrays.sort(array, new AscendingPlusAge());
 
-        @Override
-        public boolean equals(Object o) {
-            return o instanceof T && ((T) o).i == i && ((T) o).s.equals(s);
-        }
+    int i = 0;
+    for (T t : ts.innerValues()) {
+      System.err.println(t + " vs " + array[i]);
+      assertEquals("correct order at position " + i, array[i], t);
+      i++;
+    }
+    assertEquals("all elements iterated", array.length, i);
+  }
+
+  @Test
+  public void innerValuesDescending() throws Exception {
+    MultiTreeMap<Integer, T> ts = new MultiTreeMap<>((Integer a, Integer b) -> b.compareTo(a));
+    T[] array = new T[]{
+        new T(3, "3"),
+        new T(1, "1"),
+        new T(2, "2"),
+        new T(3, "3.1"),
+        new T(1, "1.1"),
+        new T(2, "2.1"),
+        new T(3, "3.2"),
+        new T(0, "0"),
+        new T(2, "2.2")};
+
+    for (T t : array) ts.putValue(t.i, t);
+    Arrays.sort(array, new DescendingPlusAge());
+
+    int i = 0;
+    for (T t : ts.innerValues()) {
+      System.err.println(t + " vs " + array[i]);
+      assertEquals("correct order at position " + i, array[i], t);
+      i++;
+    }
+    assertEquals("all elements iterated", array.length, i);
+  }
+
+  /**
+   * A simple test class with 2 fields, only the first of which is
+   * used to provide ordering
+   */
+  private static class T implements Comparable<T> {
+    private int i;
+    private String s;
+
+    public T(int i, String s) {
+      this.i = i;
+      this.s = s;
     }
 
-    private static class AscendingPlusAge implements Comparator<T> {
-        @Override
-        public int compare(T o1, T o2) {
-            int rc = o1.i - o2.i;
-            return (rc == 0) ? o1.s.compareTo(o2.s) : rc;
-        }
-    }
-    private static class DescendingPlusAge implements Comparator<T> {
-        @Override
-        public int compare(T o1, T o2) {
-            int rc = o2.i - o1.i;
-            return (rc == 0) ? o1.s.compareTo(o2.s) : rc;
-        }
+    @Override
+    public int compareTo(T o) {
+      return o.i - i;
     }
 
-    @Test
-    public void add() throws Exception {
-
+    public String toString() {
+      return "{" + i + ":" + s + "}";
     }
 
-    @Test
-    public void innerValuesAscending() throws Exception {
-        MultiTreeMap<Integer, T> ts = new MultiTreeMap<>();
-        T[] array = new T[]{
-                new T(3, "3"),
-                new T(1, "1"),
-                new T(2, "2"),
-                new T(3, "3.1"),
-                new T(1, "1.1"),
-                new T(2, "2.1"),
-                new T(3, "3.2"),
-                new T(0, "0"),
-                new T(2, "2.2")};
-
-        for (T t : array) ts.putValue(t.i, t);
-        Arrays.sort(array, new AscendingPlusAge());
-
-        int i=0;
-        for (T t : ts.innerValues()) {
-            System.err.println(t + " vs " + array[i]);
-            assertEquals("correct order at position " + i, array[i], t);
-            i++;
-        }
-        assertEquals("all elements iterated", array.length, i);
+    @Override
+    public boolean equals(Object o) {
+      return o instanceof T && ((T) o).i == i && ((T) o).s.equals(s);
     }
+  }
 
-    @Test
-    public void innerValuesDescending() throws Exception {
-        MultiTreeMap<Integer, T> ts = new MultiTreeMap<>((Integer a, Integer b)->b.compareTo(a));
-        T[] array = new T[]{
-                new T(3, "3"),
-                new T(1, "1"),
-                new T(2, "2"),
-                new T(3, "3.1"),
-                new T(1, "1.1"),
-                new T(2, "2.1"),
-                new T(3, "3.2"),
-                new T(0, "0"),
-                new T(2, "2.2")};
-
-        for (T t : array) ts.putValue(t.i, t);
-        Arrays.sort(array, new DescendingPlusAge());
-
-        int i=0;
-        for (T t : ts.innerValues()) {
-            System.err.println(t + " vs " + array[i]);
-            assertEquals("correct order at position " + i, array[i], t);
-            i++;
-        }
-        assertEquals("all elements iterated", array.length, i);
+  private static class AscendingPlusAge implements Comparator<T> {
+    @Override
+    public int compare(T o1, T o2) {
+      int rc = o1.i - o2.i;
+      return (rc == 0) ? o1.s.compareTo(o2.s) : rc;
     }
+  }
+
+  private static class DescendingPlusAge implements Comparator<T> {
+    @Override
+    public int compare(T o1, T o2) {
+      int rc = o2.i - o1.i;
+      return (rc == 0) ? o1.s.compareTo(o2.s) : rc;
+    }
+  }
 }
 
